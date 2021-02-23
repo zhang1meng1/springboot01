fbb5c784f6034f616a9b233a4004fe5daa8c9a7d
zhangmeng <zhangmeng@126.com>
2021-02-23 14:52:59 +0800

    aaa update

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:/myconfig/myconfig.properties")
 public class Springboot1Application {
-//zm-sss
+//zm-sss  aaa
     //master  ssss
     public static void main(String[] args) {
         SpringApplication.run(Springboot1Application.class, args);
